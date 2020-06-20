f2809e9cd0242a904f06acde52b09824b06065cc
Adam Gyulavari <adam.gyulavari@greenfoxacademy.com>
2020-06-20 16:55:51 +0200

    fix: use bodyparser (#2)

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const cors = require('cors');
 import { helloController } from '../controllers';
 
 const router = express.Router();
 
 router.use(cors());
-router.use(express.json());
+router.use(bodyParser.json());
 
 router.get('/hello', helloController.get);
 
