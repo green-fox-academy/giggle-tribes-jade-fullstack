376ea24dbcdef5fe8ad8ed53f47358e70cf85961
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-06-24 21:06:27 +0200

    fix: registerModel remake

--- backend/src/repos/registerModel.js
+++ backend/src/repos/registerModel.js
@@ -1,32 +1,32 @@
 import {db} from '../data/connection';
 
+const addUser = async (username,password) => {
+    return (await db.query('INSERT INTO user (name,password) VALUES(?,?)',[username,password],'insertId')).specialReturn; 
+};
+
+const addKingdom = async (kingdomname) => {
+    kingdomname = (kingdomname) ? kingdomname : `${username}'s kingdom`;
+    return (await db.query('INSERT INTO kingdom (name) VALUES(?)',[kingdomname],'insertId')).specialReturn;
+};
+
+const addUserKingdom = (userid,kingdomid) => {
+    db.query('INSERT INTO user_kingdom (user_id,kingdom_id) VALUES(?,?)',[userid,kingdomid],'');
+};
+
 export const registerModel = (username,password,kingdomname) => {
     return new Promise( async (resolve,reject) => {
-
         try {
-
-        const userid = (await db.query('INSERT INTO user (name,password) VALUES(?,?)',[username,password],'insertId')).specialReturn;
-  
-        kingdomname = (kingdomname) ? kingdomname : `${username}'s kingdom`;
-        let kingdomid = await db.query('SELECT id FROM kingdom WHERE name=?',[kingdomname],'')
-        if (kingdomid.results.length === 0) {
-            kingdomid = (await db.query('INSERT INTO kingdom (name) VALUES(?)',[kingdomname],'insertId')).specialReturn;
-        } else {
-            kingdomid = kingdomid.results[0].id;
-        }
-
-        await db.query('INSERT INTO user_kingdom (user_id,kingdom_id) VALUES(?,?)',[userid,kingdomid],'');
-
-        resolve({
-            'id' : userid,
-            'username' : username,
-            'kingdomId' : kingdomid
-        });
-
+            const userid = await addUser(username,password);
+            const kingdomid = await addKingdom(kingdomname);
+            addUserKingdom(userid,kingdomid);
+            resolve({
+                'id' : userid,
+                'username' : username,
+                'kingdomId' : kingdomid
+            });
         } catch(error) {
             if (error.code === 'ER_DUP_ENTRY') reject("Username is already taken.");
             reject(error);
         }
-
     });
 };
