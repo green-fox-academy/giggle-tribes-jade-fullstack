6b087020d64738b1948fc9d33e718aa35551aef9
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-06-25 21:55:38 +0200

    GTJ-28 SC4-5 returns the correct response, no test

--- backend/src/models/allFieldsProvided.js
+++ backend/src/models/allFieldsProvided.js
@@ -1,21 +1,19 @@
-import { checkDatabase } from './checkDatabase';
+import { checkUser } from './checkDatabase';
 const jwt = require('jsonwebtoken');
 
 export const allFieldsProvided = {
   async checkLogin(username, password) {
-    checkDatabase
-      .checkLogin(username, password, result => {
-        if (result.success) {
-          console.log(result);
-          return {
-            status: 'ok',
-            token: jwt.sign({ username: username }, process.env.JWT_SECRET),
-          };
-        } else {
-          console.log(result);
-          return { error: 'Username or password is incorrect.' };
-        }
-      })
-      .then(response => console.log('Then' + response));
+    const result = await checkUser(username, password);
+
+    if (result > 0) {
+      console.log(result);
+      return {
+        status: 'ok',
+        token: jwt.sign({ username: username }, process.env.JWT_SECRET),
+      };
+    } else {
+      console.log(result);
+      return { error: 'Username or password is incorrect.' };
+    }
   },
 };
--- backend/src/models/checkDatabase.js
+++ backend/src/models/checkDatabase.js
@@ -1,24 +1,10 @@
 import { db } from '../data/connection';
 
-export const checkDatabase = {
-  async checkLogin(username, password, callback) {
-    let result = db.query(
-      `select * from users where user_name = ? AND password = ?;`,
-      [username, password]
-    );
+export const checkUser = async (username, password) => {
+  const result = await db.query(
+    `select * from user where username = ? AND password = ?;`,
+    [username, password]
+  );
 
-    result
-      .then(response => {
-        return response.results;
-      })
-      .then(response => {
-        console.log(response);
-        if (response) {
-          callback({ success: true });
-        } else {
-          callback({ success: false });
-        }
-      })
-      .catch(error => console.log(error.code));
-  },
+  return result.results.length;
 };
