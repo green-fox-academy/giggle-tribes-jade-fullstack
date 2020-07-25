9bb0a6e1e94f116a52f67af1e155173b4b731bab
kkeszthe <55203445+kkeszthe@users.noreply.github.com>
2020-07-25 13:57:53 +0200

    GTJ-5 (#8)
    * feat: header done with test
    
    * fix: missed update of package.json and yarn.lock
    
    * fix: removed reference to missing icons, fixed according to change request
    
    * fix: removed surplus semicolon and unused variable
    
    * fix: removed unnecessary if
    
    * fix: corrected input ref

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import './App.css';
-import Resource from './components/resource/Resource';
-import Menu from './components/Menu';
-import Login from './components/Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { menuItems } from './components/menuItemsStorage';
 
+import './App.css';
+import Header from './components/header/Header';
+import Resource from './components/resource/Resource';
+import Menu from './components/Menu';
+import Login from './components/Login';
+
 function App() {
+  const kingdomName = 'Dummy';
   const kingdomID = 1;
   return (
     <div className="App">
+      <Header kingdomName={kingdomName} />
       <Router>
         <Switch>
           <Route exact path="/">
---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import './Header.css';
+import HeaderTitle from './HeaderTitle';
+
+const loggedinHeaderItems = [
+  { link: 'Settings', route: '/settings' },
+  { link: 'Logout', route: '/logout' },
+];
+const loggedoutHeaderItems = [
+  { link: 'Login', route: '/login' },
+  { link: 'Register', route: '/register' },
+];
+
+export default function ({ kingdomName }) {
+  let headerItems = loggedoutHeaderItems;
+  const isToken = localStorage.getItem('TRIBES_TOKEN') ? true : false;
+
+  if (isToken && kingdomName) {
+    headerItems = loggedinHeaderItems;
+  }
+  return (
+    <Router>
+      <nav className="header">
+        <HeaderTitle name={isToken && kingdomName} route="/buildings" />
+        <div className="header">
+          {headerItems.map((item, index) => (
+            <Link className="header" key={index.toString()} to={item.route}>
+              {item.link}
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </Router>
+  );
+}
