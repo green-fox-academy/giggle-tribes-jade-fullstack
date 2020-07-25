dacd4f28236e5ebea37ef7e9d886d9a0b40c1304
kkeszthe <55203445+kkeszthe@users.noreply.github.com>
2020-07-25 13:22:43 +0200

    GTJ-3 (#7)
    * fix: faulty mocking in session tests, faulty sessionService.js
    
    * Resource get endpoint and middleware with tests
    
    * fix: switched kingdom/resource endpoint to kingdom/:kingdomID/resource and update all components accordingly
    
    * feat: Resource frontend with 2 tests
    
    * fix: Resource function to receive object and App.js to store kingdomid as single value
    
    * Added prop-types
    
    * fix: moving token to service, including creating resource in registration
    
    * fix: added mock of autService in app-resource test
    
    * Added missing App.css, and removed references to missing icons
    
    * fix: added replaced fixed token in auth with localstorage, added token from local storage to resource header
    
    * routes moved resourceMW after authorization, corrected reference to token content as token contains userID and not userId, etc.
    
    * fix: added missing mocks
    
    * fix: optimized test
    
    * fix: removed unnecessary comment

---
+++ backend/test/app-resource.test.js
@@ -0,0 +1,76 @@
+import request from 'supertest';
+
+import app from '../src/app';
+import { getResourceForKingdom } from '../src/repos/resource';
+jest.mock('../src/repos/resource');
+jest.mock('../src/services/authService');
+import { authService } from '../src/services/authService';
+
+authService.mockImplementation(() => ({
+  userId: '44',
+  kingdomId: '26',
+}));
+
+test('should respond with 400 - Resource for this kingdom not found.', done => {
+  getResourceForKingdom.mockImplementation(async () => {
+    return Promise.resolve([]);
+  });
+
+  request(app)
+    .get('/api/kingdom/1/resource')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .end((err, data) => {
+      if (err) return done(err);
+      expect(data.body.error).toBe(
+        'UpdateResource failed. Resource for this kingdom not found.'
+      );
+      return done();
+    });
+});
+
+test('should respond with 200 - Resource for this kingdom found.', done => {
+  getResourceForKingdom.mockImplementation(async () => {
+    return Promise.resolve([
+      {
+        type: 'food',
+        amount: 500,
+        generation: 1,
+        updatedAt: '2020-07-04T08:45:00.000Z',
+      },
+      {
+        type: 'gold',
+        amount: 500,
+        generation: 1,
+        updatedAt: '2020-07-04T08:45:00.000Z',
+      },
+    ]);
+  });
+
+  request(app)
+    .get('/api/kingdom/1/resource')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, data) => {
+      if (err) return done(err);
+      expect(data.body).toStrictEqual({
+        resources: [
+          {
+            type: 'food',
+            amount: 500,
+            generation: 1,
+            updatedAt: '2020-07-04T08:45:00.000Z',
+          },
+          {
+            type: 'gold',
+            amount: 500,
+            generation: 1,
+            updatedAt: '2020-07-04T08:45:00.000Z',
+          },
+        ],
+      });
+      return done();
+    });
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,38 @@
 import React from 'react';
+import './App.css';
+import Resource from './components/resource/Resource';
 import Menu from './components/Menu';
 import Login from './components/Login';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { menuItems } from './components/menuItemsStorage';
 
 function App() {
-
+  const kingdomID = 1;
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/'>
+          <Route exact path="/">
             <Login />
           </Route>
-          <Route path='/login'>
+          <Route path="/login">
             <Login />
           </Route>
-          <Route path='/kingdom'>
-            <Menu menuItems={menuItems}/>
-            {menuItems.map( (menuItem) => (
-              <Route key={menuItem.link} path={'/kingdom' + menuItem.link} render={ () => (<menuItem.component name={menuItem.name} />) } />
+          <Route path="/kingdom">
+            <Menu menuItems={menuItems} />
+            {menuItems.map(menuItem => (
+              <Route
+                key={menuItem.link}
+                path={'/kingdom' + menuItem.link}
+                render={() => <menuItem.component name={menuItem.name} />}
+              />
             ))}
+            <Resource kingdomID={kingdomID} />
           </Route>
-        </Switch> 
+        </Switch>
       </Router>
     </div>
   );
-};
+}
 
 export default App;
