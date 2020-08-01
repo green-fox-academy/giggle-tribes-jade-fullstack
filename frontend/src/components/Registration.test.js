2cc78b48d240756dec3b36c37d1e286d53c518a6
zvorsi <56042401+zvorsi@users.noreply.github.com>
2020-08-01 10:21:15 +0200

    GTJ-25 snapshot test passed

--- frontend/src/components/Registration.test.js
+++ frontend/src/components/Registration.test.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Registration from './Registration';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { act } from "react-dom/test-utils";
 
-test('renders registration page', () => {
-    const { getByText } = render(
-      <Router>
-        <Route path='/resgistration'>
-          <MenuItem name='test' link='/test'/>
-        </Route>
-      </Router>
-    );
-    const element = getByText(/test/i);
-    expect(element).toBeInTheDocument();
-  });
+const container = document.createElement('div');
+document.body.appendChild(container);
 
+it('duma', async() =>{
+    await render(
+        <Router>
+            <Route path = './registration'>
+                <Registration />, container
+            </Route>
+        </Router>);
+});
