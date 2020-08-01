a37fc29f106a128cda969d51b4b0f488bc7aae18
zvorsi <56042401+zvorsi@users.noreply.github.com>
2020-08-01 17:47:01 +0200

    GTJ-25 page rendering tests are done

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -9,7 +9,7 @@ class Registration extends Component {
 
     constructor(props) {
         super(props);
-        console.log(props)
+        // console.log(props)
         this.state = {
             newUser: {
               username: '',
--- frontend/src/components/Registration.test.js
+++ frontend/src/components/Registration.test.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Registration from './Registration';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router as Router, Route } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
-const container = document.createElement('div');
-document.body.appendChild(container);
+const history = createMemoryHistory({
+    initialEntries: ['/']
+  });
 
-it('renders registration page', async() =>{
-    await render(
-        <Router>
-            <Route path = './registration'>
-                <Registration />, container
-            </Route>
-        </Router>);
+
+let utils =  ()=>{return render(
+    <Router history={history}>
+        <Route path = '/'>
+            <Registration />
+        </Route>
+    </Router>
+);}
+
+let fieldByPlaceholderName = (placeholder) => {return utils().getByPlaceholderText(placeholder)}
+
+it('username or kingdomname field is blank line is red', async() =>{
+    let field = fieldByPlaceholderName("Username" || "Kingdomname")
+    fireEvent.change(field, {target: {value: ""}})
+    expect(field.className).toBe("red")
 });
+
+it('username or kingdomname field is not blank line is green', async() =>{
+    let field = fieldByPlaceholderName("Username" || "Kingdomname")
+    fireEvent.change(field, {target: {value: "a"}})
+    expect(field.className).toBe("green")
+});
+
+it('password field is min 8 chars line is green', async() =>{
+    let passwordField = fieldByPlaceholderName("Password")
+    fireEvent.change(passwordField, {target: {value: "12345678"}})
+    expect(passwordField.className).toBe("green")
+});
+
+it('password field is less then 8 chars line is red', async() =>{
+    let passwordField = fieldByPlaceholderName("Password")
+    fireEvent.change(passwordField, {target: {value: "1234567"}})
+    expect(passwordField.className).toBe("red")
+});
+
+
+
