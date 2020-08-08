0a38c5076fcf5f53069860d4568f64a0892752a5
zvorsi <56042401+zvorsi@users.noreply.github.com>
2020-08-08 16:20:28 +0200

    GTJ-10 buildings frontend; endpoint kingdoms/building

--- frontend/src/components/Buildings.js
+++ frontend/src/components/Buildings.js
@@ -16,12 +16,37 @@ function Buildings(props){
       
         return (
           <div className="buildingBox">
+            
             <div className="buildingList">
-              <img src="../buildings/townhall.png" alt="townhall"></img>
-              <img src="../buildings/farm.png" alt="farm"></img>
-              <img src="../buildings/barracks.png" alt="academy"></img>
-              <img src="../buildings/mine.png" alt="mine"></img>
+
+              <div>
+                <img src="../buildings/townhall.jpg" alt="townhall"></img>
+                <img src="../buildings/farm.png" alt="farm"></img>
+                <img src="../buildings/barracks.png" alt="academy"></img>
+                <img src="../buildings/mine.png" alt="mine"></img>
+              </div>
+
+              <div>
+                <p>townhall Level 1</p>
+                <p>farm Level 1</p>
+                <p>academy Level 1</p>
+                <p>mine Level 1</p>
+              </div>
+            
+              <div>
+                <img src="../buildings/addfarm.png" alt="add farm"></img>
+                <img src="../buildings/addacademy.png" alt="add academy"></img>
+                <img src="../buildings/addmine.png" alt="add mine"></img>
+              </div>
+
+              <div>
+                <p>add farm</p>
+                <p>add academy</p>
+                <p>add mine</p>
+              </div>
+
             </div>
+
           </div>
         );
       }
