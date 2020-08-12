4949966b73140be172399b72711e31582a5ebcf6
zvorsi <56042401+zvorsi@users.noreply.github.com>
2020-08-12 20:47:52 +0200

    GTJ-25 error message from backend shows next to submit button

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -10,7 +10,8 @@ class Registration extends Component {
         this.state = {
               username: '',
               password: '',
-              kingdomname: ''            
+              kingdomname: '',
+              error: ''            
           };
           this.handleSubmit = this.handleSubmit.bind(this);
           this.handleUsername = this.handleUsername.bind(this);
@@ -38,8 +39,10 @@ class Registration extends Component {
                 ).then(data => {
                   
                   if(data.error){
-                    // console.log(data.error)
-                    history.push('/registration');
+                    this.setState(prevState => (
+                      {...prevState, error: data.error}
+                    ))
+                    // history.push('/registration');
                   }else{
                     history.push('/login');
                   }
@@ -97,23 +100,26 @@ class Registration extends Component {
                     <div>
                     <input type="text" name="username" className={this.validUsername() ? "green" : "red"} placeholder="Username" onChange = {this.handleUsername}>
                     </input>
-                    {!this.validUsername() && <div className="redimage"><span>Username must be at least 1 character long!</span></div>}
+                    {/* {!this.validUsername() && <div className="redimage"><span>Username must be at least 1 character long!</span></div>} */}
                     </div>
 
                     <div>
                     <input type="password" name="password" className={this.validPassword() ? "green" : "red"} placeholder="Password" onChange = {this.handlePassword}>
                     </input>
-                    {!this.validPassword() && <div className="redimage"><span>Password must be at least 8 characters long!</span></div>}
+                    {/* {!this.validPassword() && <div className="redimage"><span>Password must be at least 8 characters long!</span></div>} */}
                     </div>
 
                     <div className="kingdom">
                     <input type="text" name="kingdomname" className={this.validKingdomname() ? "green" : "red"} placeholder="Kingdom name" onChange = {this.handleKingdomname}>
                     </input>
-                    {!this.validKingdomname() && <div className="redimage"><span>Kingdom name must be at least 1 character long!</span></div>}
+                    {/* {!this.validKingdomname() && <div className="redimage"><span>Kingdom name must be at least 1 character long!</span></div>} */}
                     </div>
 
                     <div className="submitLine">
+                    {this.state.error && <div className="errorMessage">{this.state.error}</div>}
                     <button type="submit" className="button">SIGN UP</button>
+                    
+                    
                     </div>
 
                 </form>
