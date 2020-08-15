777a1c6d422be670965498f390de96d6dd45e9f7
zvorsi <56042401+zvorsi@users.noreply.github.com>
2020-08-15 10:01:38 +0200

    GTJ-25 fix conflicts 3

--- frontend/src/components/resource/Resource.test.js
+++ frontend/src/components/resource/Resource.test.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
-
-import Resource from './Resource';
-
-global.fetch = jest.fn(async () => {
-  return await Promise.resolve({
-    json: () =>
-      Promise.resolve({
-        resources: [
-          {
-            type: 'food',
-            amount: 6824,
-            generation: 1,
-            updatedAt: '2020-07-08T18:32:01.000Z',
-          },
-          {
-            type: 'gold',
-            amount: 6824,
-            generation: 1,
-            updatedAt: '2020-07-08T18:32:01.000Z',
-          },
-        ],
-      }),
-  });
-});
-
-let container;
-
-beforeEach(() => {
-  fetch.mockClear();
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it('renders Resource without crashing', async () => {
-  await act(async () => {
-    render(<Resource kingdomID={1} />, container);
-  });
-});
-
-it('matches snapeshot', async () => {
-  await act(async () => {
-    render(<Resource kingdomID={1} />, container);
-  });
-
-  expect(container.innerHTML).toMatchSnapshot();
-});
-=======
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render, unmountComponentAtNode } from 'react-dom';
@@ -143,4 +86,3 @@ it('matches snapeshot', async () => {
 
   expect(container.innerHTML).toMatchSnapshot();
 });
->>>>>>> 0e29a74ac91b26588602107059eaa740036b78a7
