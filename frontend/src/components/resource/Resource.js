84d2062c987f6b564ea5d7d554f8ddd9624edc77
zvorsi <56042401+zvorsi@users.noreply.github.com>
2020-08-15 09:57:57 +0200

    GTJ-25 fix conflicts 2

--- frontend/src/components/resource/Resource.js
+++ frontend/src/components/resource/Resource.js
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import './Resource.css';
-import Farm from '../../assets/buildings/farm.svg';
-import Mine from '../../assets/buildings/mine.svg';
-import Food from '../../assets/sources/FoodIcon.svg';
-import Gold from '../../assets/sources/GoldIcon.svg';
-
-export default function Resource({ kingdomID }) {
-  const [foodAmount, setFoodAmount] = useState(null);
-  const [goldAmount, setGoldAmount] = useState(null);
-
-  const getResources = async kingdomID => {
-    await fetch(`http://localhost:5000/api/kingdom/${kingdomID}/resource`, {
-      headers: {
-        'Content-Type': 'application/json',
-        TRIBES_TOKEN: localStorage.getItem('TRIBES_TOKEN'),
-      },
-    })
-      .then(res => res.json())
-      .then(response => response.resources)
-      .then(resources =>
-        resources.map(async resource => {
-          if (resource.type === 'food') {
-            setFoodAmount(resource.amount);
-          } else {
-            setGoldAmount(resource.amount);
-          }
-        })
-      )
-      .catch(console.log);
-  };
-
-  useEffect(() => {
-    getResources(kingdomID);
-  }, [kingdomID]);
-
-  return (
-    <div className="resources">
-      <img className="resource farm" src={Farm} alt="farm icon" />
-      <div className="resource food">
-        <div>
-          <h1>{foodAmount}</h1>
-          <img className="food" src={Food} alt="food icon" />
-        </div>
-        <p>{} / minute</p>
-      </div>
-      <img className="resource mine" src={Mine} alt="mine icon" />
-      <div className="resource gold">
-        <div>
-          <h1>{goldAmount}</h1>
-          <img className="gold" src={Gold} alt="gold icon" />
-        </div>
-        <p>{} / minute</p>
-      </div>
-    </div>
-  );
-}
-
-Resource.propTypes = {
-  kingdomID: PropTypes.any.isRequired,
-};
-=======
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -127,4 +63,3 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Resource);
->>>>>>> 0e29a74ac91b26588602107059eaa740036b78a7
