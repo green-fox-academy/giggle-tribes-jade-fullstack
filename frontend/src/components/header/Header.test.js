186e5b35fe1cf9a90bee0026ab60e040f7e9df33
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-08-18 19:42:37 +0200

    fix: Header and App tests fixed to Redux

--- frontend/src/components/header/Header.test.js
+++ frontend/src/components/header/Header.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
+import configureStore from 'redux-mock-store';
 
 import Header from './Header';
+import { store } from '../../store';
 
 let container;
 
@@ -18,41 +21,70 @@ afterEach(() => {
 });
 
 it('renders Header without crashing without token', async () => {
+  const mockStore = configureStore([]);
+  const mockedStore = mockStore({
+  });
+  mockedStore.dispatch = jest.fn();
   Storage.prototype.getItem = jest.fn(key => {
-    return;
   });
   await act(async () => {
-    render(<Header />, container);
+    render(
+    <Provider store={mockedStore}>
+        <Header />
+    </Provider>,
+    container
+    );
   });
 });
 
 it('renders Header without crashing with token but without kingdomName', async () => {
+  const mockStore = configureStore([]);
+  const mockedStore = mockStore({
+  });
+  mockedStore.dispatch = jest.fn();
   Storage.prototype.getItem = jest.fn(key => {
     return 'dummy_token';
   });
   await act(async () => {
-    render(<Header />, container);
+    render(
+    <Provider store={mockedStore}>
+        <Header />
+    </Provider>,
+    container
+    );
   });
 });
 
-it('renders Header without crashing with token and kingdomName', async () => {
+it('renders Header without crashing with token and kingdomId', async () => {
   Storage.prototype.getItem = jest.fn(key => {
     return 'dummy_token';
   });
+  const mockStore = configureStore([]);
+  const mockedStore = mockStore({
+    kingdom: 1
+  });
+  mockedStore.dispatch = jest.fn();
   await act(async () => {
-    render(<Header kingdomName={'Test'} />, container);
+    render(
+    <Provider store={mockedStore}>
+        <Header />
+    </Provider>,
+    container
+    );
   });
 });
 
 it('matches snapshot without token', async () => {
   Storage.prototype.getItem = jest.fn(key => {
-    return;
   });
-
   await act(async () => {
-    render(<Header />, container);
+    render(
+    <Provider store={store}>
+        <Header />
+    </Provider>,
+    container
+    );
   });
-
   expect(container.innerHTML).toMatchSnapshot();
 });
 
@@ -60,22 +92,37 @@ it('matches snapshot with token without kingdomID', async () => {
   Storage.prototype.getItem = jest.fn(key => {
     return 'dummy_token';
   });
-
-  await act(async () => {
-    render(<Header />, container);
+  const mockStore = configureStore([]);
+  const mockedStore = mockStore({
   });
-
+  mockedStore.dispatch = jest.fn();
+  await act(async () => {
+    render(
+    <Provider store={mockedStore}>
+        <Header />
+    </Provider>,
+    container
+    );
+  });
   expect(container.innerHTML).toMatchSnapshot();
 });
 
-it('matches snapshot with token and kingdomName', async () => {
+it('matches snapshot with token and kingdomId', async () => {
   Storage.prototype.getItem = jest.fn(key => {
     return 'dummy_token';
   });
-
-  await act(async () => {
-    render(<Header kingdomName="Test" />, container);
+  const mockStore = configureStore([]);
+  const mockedStore = mockStore({
+    kingdom: 1,
   });
-
+  mockedStore.dispatch = jest.fn();
+  await act(async () => {
+    render(
+    <Provider store={mockedStore}>
+        <Header />
+    </Provider>,
+    container
+    );
+  });
   expect(container.innerHTML).toMatchSnapshot();
 });
