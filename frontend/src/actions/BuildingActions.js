1284fd8597ccb2f2881db35681349b8754118600
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-08-18 20:15:25 +0200

    feat: actions buildings and troops

---
+++ frontend/src/actions/BuildingActions.js
@@ -0,0 +1,48 @@
+import {
+  UPDATE_BUILDINGS,
+  UPDATE_BUILDINGS_SUCCESS,
+  ADD_BUILDING,
+  ADD_BUILDING_SUCCESS,
+  SET_ERROR_SUCCESS,
+} from '../constants/ActionTypes';
+
+import { fetchKingdom } from '../services/fetchKindom';
+
+export const getBuildingsAction = kingdomID => {
+  return dispatch => {
+    dispatch({
+      type: UPDATE_BUILDINGS,
+    });
+
+    return fetchKingdom.get(kingdomID, 'buildings').then(
+      response =>
+        response.error
+          ? dispatch({ type: SET_ERROR_SUCCESS, payload: response.error })
+          : dispatch({
+              type: UPDATE_BUILDINGS_SUCCESS,
+              payload: response.troops,
+            }),
+      error => dispatch({ type: SET_ERROR_SUCCESS, payload: error })
+    );
+  };
+};
+
+export const addTroopAction = kingdomID => {
+  return dispatch => {
+    dispatch({
+      type: ADD_BUILDING,
+    });
+
+    return fetchKingdom.post(kingdomID, 'buildings', type).then(
+      response => {
+        response.error
+          ? dispatch({ type: SET_ERROR_SUCCESS, payload: response.error })
+          : dispatch({
+              type: ADD_BUILDING_SUCCESS,
+              payload: response,
+            });
+      },
+      error => dispatch({ type: SET_ERROR_SUCCESS, payload: error.error })
+    );
+  };
+};
