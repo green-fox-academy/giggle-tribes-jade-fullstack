9717fab91f7d2b2181399a83fd261afb07d6902c
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-08-19 22:28:49 +0200

    fix: change requests

--- backend/src/services/buildingService.js
+++ backend/src/services/buildingService.js
@@ -9,7 +9,7 @@ const errorMessages = {
   invalidId: 4
 };
 
-const defaultData = {
+const buildingStats = {
   farm : { hp : 1, cost : 100, time : 60000, gen : 5, genType : 'food' },
   mine : { hp : 1, cost : 100, time : 60000, gen : 5, genType : 'gold' },
   academy : { hp : 1, cost : 100, time : 60000, gen : 0, genType : '-' }
@@ -17,13 +17,13 @@ const defaultData = {
 
 const createBuildingData = (data) => {
   const start = new Date();
-  const finish = new Date( start.getTime() + defaultData[data.type].time );
+  const finish = new Date( start.getTime() + buildingStats[data.type].time );
   return {
     id : null,
     kingdomId : data.kingdomId,
     type : data.type,
     level : 1,
-    hp : defaultData[data.type].hp,
+    hp : buildingStats[data.type].hp,
     started_at : start.toLocaleString(),
     finished_at : finish.toLocaleString()
     }
@@ -50,14 +50,14 @@ const validateKingdomId = async (id) => {
   if ( kingdomBase.length === 0 ) throw new Error(errorMessages.invalidId);
 };
 
-const validateBuild = async (id,type) => {
+const getResourceChanges = async (id,type) => {
   const resources = await resourceService.getResource(id);
   const gold = resources.resources.filter(e => e.type === 'gold')[0].amount;
-  if ( gold < defaultData[type].cost ) throw new Error(errorMessages.notEnoughGold);
+  if ( gold < buildingStats[type].cost ) throw new Error(errorMessages.notEnoughGold);
   return {
-    cost: defaultData[type].cost,
-    genAdd: defaultData[type].gen,
-    genType: defaultData[type].genType
+    cost: buildingStats[type].cost,
+    genAdd: buildingStats[type].gen,
+    genType: buildingStats[type].genType
   };
 };
 
@@ -65,7 +65,7 @@ const validateBuild = async (id,type) => {
 const add = async (input) => {
     validateType(input.type);
     await validateKingdomId(input.kingdomId);
-    const resourcesData = await validateBuild({kingdomID:input.kingdomId},input.type);
+    const resourcesData = await getResourceChanges({kingdomID:input.kingdomId},input.type);
     const buildingDataInput = createBuildingData(input);
     const buildingId = await buildingRepo.add( buildingDataInput );
     switch (input.type) {
