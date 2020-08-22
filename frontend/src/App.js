352e1f59693dbfabd2bff6d2d3c69e7fccd92ac6
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-08-22 12:16:18 +0200

    fix: removed surplus variable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,6 @@ function App() {
     'TRIBES_TOKEN',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjMsImlhdCI6MTU5ODA0MTk3Mn0.cVAcR4UiobKN05oicJYQYPIkibMws4yIATb8GVuVD_g'
   );
-  const academyLevel = 1;
 
   return (
     <div className="App">
