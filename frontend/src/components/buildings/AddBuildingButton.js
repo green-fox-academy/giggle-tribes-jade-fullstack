23b91a776e13d5da6eba130e4d9cf9d0c94c6b4d
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-08-22 12:20:28 +0200

    fix: unique class name

--- frontend/src/components/buildings/AddBuildingButton.js
+++ frontend/src/components/buildings/AddBuildingButton.js
@@ -4,7 +4,7 @@ import './AddBuildingButton.css';
 const AddBuildingButton = ({ buildingData, onClick }) => {
   return (
     <div className="button" onClick={() => onClick(buildingData)}>
-      <div className={'buttonimage ' + 'add' + buildingData.type}></div>
+      <div className={'buttonimage add' + buildingData.type}></div>
       <p>
         Add{' '}
         {buildingData.type.charAt(0).toUpperCase() + buildingData.type.slice(1)}
