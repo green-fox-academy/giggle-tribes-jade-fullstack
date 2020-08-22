fe2a1b81bc7b15f2a55af483ba12600ac5536a4d
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-08-22 11:10:11 +0200

    fix: added Router to test

--- frontend/src/components/header/Header.test.js
+++ frontend/src/components/header/Header.test.js
@@ -3,11 +3,12 @@ import { Provider } from 'react-redux';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import configureStore from 'redux-mock-store';
+import { Router, Route } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 jest.mock('../.././services/fetchKindom');
-import {fetchKingdom} from '../.././services/fetchKindom';
+import { fetchKingdom } from '../.././services/fetchKindom';
 
 import Header from './Header';
-import { store } from '../../store';
 
 let container;
 
@@ -25,51 +26,57 @@ afterEach(() => {
 it('renders Header without crashing without token', async () => {
   const mockStore = configureStore([]);
   const mockedStore = mockStore({
-    kingdom: 1
+    kingdom: 1,
   });
   mockedStore.dispatch = jest.fn();
-  Storage.prototype.getItem = jest.fn(key => {
-  });
-  fetchKingdom.get.mockImplementation( () => Promise.resolve({
-    kingdoms : [
-      {
-        kingdom_id : 1,
-        kingdomname : 'Mocked Kingdom'
-      }
-    ]
-  }) );
+  Storage.prototype.getItem = jest.fn(key => {});
+  fetchKingdom.get.mockImplementation(() =>
+    Promise.resolve({
+      kingdoms: [
+        {
+          kingdom_id: 1,
+          kingdomname: 'Mocked Kingdom',
+        },
+      ],
+    })
+  );
   await act(async () => {
     render(
-    <Provider store={mockedStore}>
-        <Header />
-    </Provider>,
-    container
+      <Provider store={mockedStore}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </Provider>,
+      container
     );
   });
 });
 
 it('renders Header without crashing with token but without kingdomName', async () => {
   const mockStore = configureStore([]);
-  const mockedStore = mockStore({
-  });
+  const mockedStore = mockStore({});
   mockedStore.dispatch = jest.fn();
   Storage.prototype.getItem = jest.fn(key => {
     return 'dummy_token';
   });
-  fetchKingdom.get.mockImplementation( () => Promise.resolve({
-    kingdoms : [
-      {
-        kingdom_id : 1,
-        kingdomname : 'Mocked Kingdom'
-      }
-    ]
-  }) );
+  fetchKingdom.get.mockImplementation(() =>
+    Promise.resolve({
+      kingdoms: [
+        {
+          kingdom_id: 1,
+          kingdomname: 'Mocked Kingdom',
+        },
+      ],
+    })
+  );
   await act(async () => {
     render(
-    <Provider store={mockedStore}>
-        <Header />
-    </Provider>,
-    container
+      <Provider store={mockedStore}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </Provider>,
+      container
     );
   });
 });
@@ -80,23 +87,27 @@ it('renders Header without crashing with token and kingdomId', async () => {
   });
   const mockStore = configureStore([]);
   const mockedStore = mockStore({
-    kingdom: 1
+    kingdom: 1,
   });
   mockedStore.dispatch = jest.fn();
-  fetchKingdom.get.mockImplementation( () => Promise.resolve({
-    kingdoms : [
-      {
-        kingdom_id : 1,
-        kingdomname : 'Mocked Kingdom'
-      }
-    ]
-  }) );
+  fetchKingdom.get.mockImplementation(() =>
+    Promise.resolve({
+      kingdoms: [
+        {
+          kingdom_id: 1,
+          kingdomname: 'Mocked Kingdom',
+        },
+      ],
+    })
+  );
   await act(async () => {
     render(
-    <Provider store={mockedStore}>
-        <Header />
-    </Provider>,
-    container
+      <Provider store={mockedStore}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </Provider>,
+      container
     );
   });
 });
@@ -107,23 +118,27 @@ it('matches snapshot without token', async () => {
   });
   const mockStore = configureStore([]);
   const mockedStore = mockStore({
-    kingdom: 1
+    kingdom: 1,
   });
   mockedStore.dispatch = jest.fn();
-  fetchKingdom.get.mockImplementation( () => Promise.resolve({
-    kingdoms : [
-      {
-        kingdom_id : 1,
-        kingdomname : 'Mocked Kingdom'
-      }
-    ]
-  }) );
+  fetchKingdom.get.mockImplementation(() =>
+    Promise.resolve({
+      kingdoms: [
+        {
+          kingdom_id: 1,
+          kingdomname: 'Mocked Kingdom',
+        },
+      ],
+    })
+  );
   await act(async () => {
     render(
-    <Provider store={mockedStore}>
-        <Header />
-    </Provider>,
-    container
+      <Provider store={mockedStore}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </Provider>,
+      container
     );
   });
   expect(container.innerHTML).toMatchSnapshot();
@@ -134,23 +149,26 @@ it('matches snapshot with token without kingdomID', async () => {
     return 'dummy_token';
   });
   const mockStore = configureStore([]);
-  const mockedStore = mockStore({
-  });
+  const mockedStore = mockStore({});
   mockedStore.dispatch = jest.fn();
-  fetchKingdom.get.mockImplementation( () => Promise.resolve({
-    kingdoms : [
-      {
-        kingdom_id : 1,
-        kingdomname : 'Mocked Kingdom'
-      }
-    ]
-  }) );
+  fetchKingdom.get.mockImplementation(() =>
+    Promise.resolve({
+      kingdoms: [
+        {
+          kingdom_id: 1,
+          kingdomname: 'Mocked Kingdom',
+        },
+      ],
+    })
+  );
   await act(async () => {
     render(
-    <Provider store={mockedStore}>
-        <Header />
-    </Provider>,
-    container
+      <Provider store={mockedStore}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </Provider>,
+      container
     );
   });
   expect(container.innerHTML).toMatchSnapshot();
@@ -165,20 +183,24 @@ it('matches snapshot with token and kingdomId', async () => {
     kingdom: 1,
   });
   mockedStore.dispatch = jest.fn();
-  fetchKingdom.get.mockImplementation( () => Promise.resolve({
-    kingdoms : [
-      {
-        kingdom_id : 1,
-        kingdomname : 'Mocked Kingdom'
-      }
-    ]
-  }) );
+  fetchKingdom.get.mockImplementation(() =>
+    Promise.resolve({
+      kingdoms: [
+        {
+          kingdom_id: 1,
+          kingdomname: 'Mocked Kingdom',
+        },
+      ],
+    })
+  );
   await act(async () => {
     render(
-    <Provider store={mockedStore}>
-        <Header />
-    </Provider>,
-    container
+      <Provider store={mockedStore}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </Provider>,
+      container
     );
   });
   expect(container.innerHTML).toMatchSnapshot();
