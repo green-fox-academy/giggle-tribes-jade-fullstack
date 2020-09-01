528db770e482790f11ae9e21136df7c43918742f
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-09-01 16:41:59 +0200

    fix: removed logic from TroopsRepo.update

--- backend/src/repos/TroopsRepo.js
+++ backend/src/repos/TroopsRepo.js
@@ -39,10 +39,28 @@ class TroopsRepo {
     return troop.results;
   }
 
-  async update(kingdomID, level, started_at, finished_at) {
+  async update(
+    kingdomID,
+    newLevel,
+    oldLevel,
+    hp,
+    attack,
+    defence,
+    started_at,
+    finished_at
+  ) {
     const troop = await db.query(
-      `update ${this.tableName}  set ${this.columns.level} = ${this.columns.level} + 1, ${this.columns.hp} = ${this.columns.hp} + 1, ${this.columns.attack} = ${this.columns.attack} + 1, ${this.columns.defence} = ${this.columns.defence} + 1, ${this.columns.started_at}=?, ${this.columns.finished_at}=? where ${this.columns.kingdom_id} = ? and ${this.columns.level} = ? LIMIT 1;`,
-      [started_at, finished_at, kingdomID, level]
+      `update ${this.tableName} set ${this.columns.level}=?, ${this.columns.hp}=?, ${this.columns.attack}=?, ${this.columns.defence}=?, ${this.columns.started_at}=?, ${this.columns.finished_at}=? where ${this.columns.kingdom_id} = ? and ${this.columns.level} = ? LIMIT 1;`,
+      [
+        newLevel,
+        hp,
+        attack,
+        defence,
+        started_at,
+        finished_at,
+        kingdomID,
+        oldLevel,
+      ]
     );
     return troop.results;
   }
