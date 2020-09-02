99fa05dedc9cd542eaa3d453c0c519eabe32ff05
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-09-02 19:01:09 +0200

    fix: removed unnecessary redux

--- frontend/src/components/troops/TroopButton.js
+++ frontend/src/components/troops/TroopButton.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import './TroopButton.css';
-import { setErrorAction } from '../../actions/ErrorActions';
 import Troop from '../../assets/icons/troops.svg';
 
-function TroopButton({ troopSummary }) {
+export default function TroopButton({ troopSummary }) {
   return (
     <Button className="troops button" onClick={() => {}}>
       <img className="troops button image" src={Troop} alt="troop icon" />
@@ -17,18 +15,5 @@ function TroopButton({ troopSummary }) {
 }
 
 TroopButton.propTypes = {
-  kingdom: PropTypes.any.isRequired,
+  troopSummary: PropTypes.any.isRequired,
 };
-
-const mapStateToProps = state => {
-  return state;
-};
-const mapDispatchToProps = dispatch => {
-  return {
-    setError: error => {
-      dispatch(setErrorAction(error));
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(TroopButton);
