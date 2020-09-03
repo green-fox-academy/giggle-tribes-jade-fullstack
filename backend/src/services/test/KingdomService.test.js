2dda842fecf28d3e8fc613f89d3f704872cf7ae0
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-09-03 20:22:18 +0200

    test: KingdomService test extend

--- backend/src/services/test/KingdomService.test.js
+++ backend/src/services/test/KingdomService.test.js
@@ -82,9 +82,68 @@ class KingdomRepo {
   };
 };
 
+class KingdomRepo_notnullLocation {
+  getById({kingdomId}) {
+    return [{
+        kingdomId: kingdomId,
+        kingdomName: 'firstKingdom',
+        userId: 2,
+        location: 'STB'
+    }];
+  };
+  async get() {
+    return [
+      {
+        kingdomId: 1,
+        kingdomName: 'firstKingdom',
+        location: 'ABC'
+      },
+      {
+        kingdomId: 2,
+        kingdomName: 'secondKingdom',
+        location: 'DEF'
+      },
+  ];
+  };
+};
+
 const kingdom = new KingdomService({KingdomRepo,ResourceRepo,BuildingRepo,LocationRepo,db,errorCodes});
 
-test('add: valid userdata returns valid result', async () => {
+test('attachLocation: missing kingdomId returns error 104', async () => {
+  try {
+    const result = await kingdom.attachLocation({locationCode: 'TST'}); 
+  } catch(err) {
+    expect(err).toStrictEqual( Error(104) );
+  }
+});
+
+test('attachLocation: missing locationCode returns error 109', async () => {
+  try {
+    const result = await kingdom.attachLocation({kingdomId: 13}); 
+  } catch(err) {
+    expect(err).toStrictEqual( Error(109) );
+  }
+});
+
+test('attachLocation: located kingdomId returns error 304', async () => {
+  const kingdom = new KingdomService({KingdomRepo:KingdomRepo_notnullLocation,ResourceRepo,BuildingRepo,LocationRepo,db,errorCodes});
+  try {
+    const result = await kingdom.attachLocation({kingdomId: 13, locationCode: 'TST'}); 
+  } catch(err) {
+    expect(err).toStrictEqual( Error(304) );
+  }
+});
+
+test('attachLocation: used location returns error 309', async () => {
+  const kingdom = new KingdomService({KingdomRepo,ResourceRepo,BuildingRepo,LocationRepo,db,errorCodes});
+  try {
+    const result = await kingdom.attachLocation({kingdomId: 13, locationCode: 'ABC'}); 
+  } catch(err) {
+    expect(err).toStrictEqual( Error(309) );
+  }
+});
+
+test('attachLocation: valid userdata returns valid result', async () => {
   const result = await kingdom.attachLocation({kingdomId: 13, locationCode: 'TST'}); 
   expect(result).toStrictEqual({
     id: 13,
@@ -127,3 +186,69 @@ test('add: valid userdata returns valid result', async () => {
   });
 });
 
+test('getById: missing kingdomId returns error 104', async () => {
+  try {
+    const result = await kingdom.getById({}); 
+  } catch(err) {
+    expect(err).toStrictEqual( Error(104) );
+  }
+});
+
+test('getById: valid userdata returns valid result', async () => {
+  const result = await kingdom.getById({kingdomId: 44}); 
+  expect(result).toStrictEqual({
+    id: 44,
+    name: 'firstKingdom',
+    userId: 2,
+    buildings: [
+      {
+        "finished_at": "2020-08-19 19:06:22",
+        "hp": 1,
+        "id": 1,
+        "kingdomId": 44,
+        "level": 1,
+        "started_at": "2020-08-19 19:06:22",
+        "type": "academy"
+      },
+      {
+        "finished_at": "2020-08-19 19:06:22",
+        "hp": 1,
+        "id": 2,
+        "kingdomId": 44,
+        "level": 4,
+        "started_at": "2020-08-19 19:06:22",
+        "type": "farm",
+      }
+    ],
+    resources: [
+      {
+        "amount": 100,
+        "generation": 5,
+        "type": "gold",
+      },
+      {
+        "amount": 200,
+        "generation": 10,
+        "type": "food",
+      }
+    ],
+    troops: [],
+    location: { "country_code": null }
+  });
+});
+
+test('get: returns valid result', async () => {
+  const result = await kingdom.get(); 
+  expect(result).toStrictEqual([
+    {
+      kingdomId: 1,
+      kingdomName: 'firstKingdom',
+      location: 'ABC'
+    },
+    {
+      kingdomId: 2,
+      kingdomName: 'secondKingdom',
+      location: 'DEF'
+    },
+  ]);
+});
