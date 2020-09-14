2bff0054ed06e39a44f19d10596deb4949e7a003
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-09-14 22:03:52 +0200

    deployment preparation

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -23,7 +23,7 @@ class Registration extends Component {
     let userData = this.state;
     const { history } = this.props;
 
-    fetch(`http://localhost:${process.env.PORT || 3000}/api/users`, {
+    fetch(`http://localhost:${3000}/api/users`, {
       method: 'POST',
       body: JSON.stringify(userData),
       headers: {
