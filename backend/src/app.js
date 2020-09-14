38754593cd1d62b4082361373918e608a9442e6d
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-09-14 21:38:47 +0200

    deployment preparation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 
 import { api, system } from './routes';
 import logger from './logger';
@@ -7,11 +8,16 @@ import errorHandler from './middlewares/error-handler';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'static/')));
 app.use(morgan('combined', { stream: logger.stream }));
 
 app.use('/api', api);
 app.use('/system', system);
 
 app.use(errorHandler);
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'static/index.html'));
+});
 
 export default app;
