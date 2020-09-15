a9f55986437e5bb9e53f5dafe82f1a9551d9d0ca
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-09-15 19:37:59 +0200

    updated registation frontend and env file

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import './registration.css';
 import { withRouter } from 'react-router-dom';
+
+import './registration.css';
+import { env } from '../env';
 
 class Registration extends Component {
   constructor(props) {
