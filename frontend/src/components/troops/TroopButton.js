8ad4bf5b154a56148a1a38e4c61636cbe4d91ec4
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-09-16 23:58:10 +0200

    Added cost info to TroopButton

--- frontend/src/components/troops/TroopButton.js
+++ frontend/src/components/troops/TroopButton.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Button } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 import './TroopButton.css';
+import Gold from '../../assets/sources/GoldIcon.svg';
 import Troop from '../../assets/icons/troops.svg';
 import TroopUpgradeBox from './TroopUpgradeBox';
 import Error from '../Error';
 
-function TroopButton({ troopSummary }) {
+export default function TroopButton({ troopSummary }) {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -16,11 +16,18 @@ function TroopButton({ troopSummary }) {
   };
 
   return (
-    <div>
-      <Button className="troops button" onClick={handleClickOpen}>
+    <div className="troopsButton">
+      <Button className="troops button" onClick={handleClickOpen} size="small">
         <img className="troops button image" src={Troop} alt="troop icon" />
         <p>{troopSummary.amount + ' Troop level ' + troopSummary.level}</p>
       </Button>
+      <p className="costLabel">
+        <p>
+          Upgrade <br />
+          10 <img className="inlineIcon" src={Gold} alt="attack icon" /> per
+          troop
+        </p>
+      </p>
       <TroopUpgradeBox open={open} setOpen={setOpen} />
       <Error />
     </div>
@@ -30,9 +37,3 @@ function TroopButton({ troopSummary }) {
 TroopButton.propTypes = {
   troopSummary: PropTypes.object.isRequired,
 };
-
-const mapStateToProps = ({ kingdom, troops }) => {
-  return { kingdom, troops };
-};
-
-export default connect(mapStateToProps)(TroopButton);
