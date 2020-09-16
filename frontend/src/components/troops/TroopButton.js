683bd777748046c56b1ff89eae58adc8460df26d
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-09-16 20:17:26 +0200

    TroopButton upgraded with Upgrade Troop pop up

--- frontend/src/components/troops/TroopButton.js
+++ frontend/src/components/troops/TroopButton.js
@@ -1,20 +1,75 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import PropTypes from 'prop-types';
 
 import './TroopButton.css';
 import Troop from '../../assets/icons/troops.svg';
-import {  } from '../../actions/TroopsActions';
+import { upgradeTroopAction } from '../../actions/TroopsActions';
+import Error from '../Error';
 
-export default function TroopButton({ troopSummary, kingdom, troops,  }) {
-  const upgradeTroop = ()
+function TroopButton({ troopSummary, kingdom, upgrade }) {
+  const [open, setOpen] = React.useState(false);
+  const [amount, setAmount] = React.useState(0);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const upgradeTroopClickOn = () => {
+    upgrade(kingdom, amount);
+    handleClose();
+  };
 
   return (
-    <Button className="troops button" onClick={() => {}}>
-      <img className="troops button image" src={Troop} alt="troop icon" />
-      <p>{troopSummary.amount + ' Troop level ' + troopSummary.level}</p>
-    </Button>
+    <div>
+      <Button className="troops button" onClick={handleClickOpen}>
+        <img className="troops button image" src={Troop} alt="troop icon" />
+        <p>{troopSummary.amount + ' Troop level ' + troopSummary.level}</p>
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Troop Upgrade</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please provide amount of troops to be upgraded and click upgrade.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Amount"
+            type="number"
+            InputProps={{ inputProps: { min: 0 } }}
+            onChange={e => {
+              setAmount(e.target.value);
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={upgradeTroopClickOn} color="primary">
+            Upgrade
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Error />
+    </div>
   );
 }
 
@@ -30,11 +85,10 @@ const mapStateToProps = ({ kingdom, troops }) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    get: kingdomId => {
-      dispatch(getTroopsAction(kingdomId));
+    upgrade: (kingdomId, amount) => {
+      dispatch(upgradeTroopAction(kingdomId, amount));
     },
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Troops);
-
+export default connect(mapStateToProps, mapDispatchToProps)(TroopButton);
