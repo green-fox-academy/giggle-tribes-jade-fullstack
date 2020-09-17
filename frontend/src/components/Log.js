bc5a118eb5742ba31c477d90cac83e094f6ac887
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-09-17 22:39:01 +0200

    feat: Log troop redux read

--- frontend/src/components/Log.js
+++ frontend/src/components/Log.js
@@ -1,23 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import LogEntry from './LogEntry';
-import { logEntryService } from '../services/logEntryService';
+import { connect } from 'react-redux';
+import { getTroopsAction } from '../actions/TroopsActions';
+import { getBuildingsAction } from '../actions/BuildingsActions';
 import './Log.css';
 
-const Log = () => {
-  const [data, setData] = useState([]);
+const Log = ({ kingdom, buildings, troops, get }) => {
 
   useEffect(() => {
-    setData(logEntryService.readLogEntries());
-  }, []);
+    get(kingdom);
+  }, [kingdom, get]);
 
+  const processTroopEntry = entry => {
+    let data = {
+      subject : 'troop',
+      action : (entry.level === 1) ? 'build' : 'upgrade',
+      level : entry.level,
+      started_at : entry.started_at,
+      finished_at : entry.finished_at
+    };
+    data.progress_at = ( new Date(data.finished_at) < new Date() ) ? data.finished_at : data.started_at;
+    return data;
+  };
+  
   return (
     <div className="log">
       <LogEntry header={true} />
-      {data.map((entry, i) => (
-        <LogEntry key={i} header={false} data={entry} />
+      {troops.map((entry, i) => (
+        <LogEntry key={i} header={false} data={processTroopEntry(entry)} />
       ))}
     </div>
   );
 };
 
-export default Log;
+/*
+{buildings.map((entry, i) => (
+        <LogEntry key={i} header={false} data={entry} />
+      ))}*/
+
+const mapStateToProps = ({ kingdom, buildings, troops }) => {
+  return { kingdom, buildings, troops };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    get: kingdomId => {
+      dispatch(getTroopsAction(kingdomId));
+      //dispatch(getBuildingsAction(kingdomId));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Log);
+
