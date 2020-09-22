83627d01b0bce2ea4fd389a4b8106891b66136af
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-09-22 20:13:24 +0200

    feat: authenticated kingdomId check add

--- backend/src/middlewares/ResourceMiddleware.js
+++ backend/src/middlewares/ResourceMiddleware.js
@@ -9,12 +9,15 @@ export class ResourceMiddleware {
             [errorCodes.missingResourceAmount]: {status: 400, message: 'Missing resource amount.'},
             [errorCodes.missingResourceGeneration]: {status: 400, message: 'Missing resource generation.'},
             [errorCodes.invalidKingdomId]: {status: 400, message: 'Invalid kingdomId.'},
+            [errorCodes.invalidToken]: {status: 403, message: 'Unauthorized for this kingdom.'},
         };
     };
 
     async post(req, res, next) {
-        const {kingdomId} = req.params;
+        const { kingdomId } = req.params;
+        const kingdomIdTarget = req.user.kingdomId;
         try {
+            if ( kingdomId != kingdomIdTarget ) throw new Error('212');
             await this.resources.generateResources({kingdomId});
             next();
         } catch(error) {
